9fae31f55b5e5619c00cd90ef6df502ec6227e06
Tharid Uralwong <47021472+BurningFlare@users.noreply.github.com>
2023-11-26 06:47:26 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+//var player = new Player(createVector(200, 200), []);
+
+function drawMap(level) {
+    for (var j = 0; j < levels[level].length; j += 1) {
+        for (var i = 0; i < levels[level][0].length; i += 1) {
+            if (levels[level][i][j] == 1) {
+                fill(50, 50, 50);
+                strokeWeight(0.1);
+                stroke(255, 255, 255);
+                rect(i * tileSize, j * tileSize, tileSize, tileSize);
+            }
+        }
+    }
+}
+
+function preload() {
+    defaultSwordSprite = loadImage("Assets/Sprites/Weapons/Default Sword.png");
+}
+
+function setup() {
+    createCanvas(width, height);
+    player = new Player(createVector(200, 200), []);
+    camera = new Camera(0, 0);
+    sword = new Sword("Excalibur", 1.5, 21, 36, player);
+}
+
+var spriteTestMode = false;
+
+function draw() {
+    
+    background(0, 0, 0);
+    
+    // translate(width/2, height/2);
+
+    // // draw sprite here
+    // //image(defaultSwordSprite, 0, 0, 21 * 10, 36 * 10);
+
+    // resetMatrix();
+
+
+    // if (spriteTestMode) {
+    //     return;
+    // }
+
+    
+    // PHYSICS UPDATES
+    // check player collision with walls
+    player.update();
+    sword.update();
+    
+    // update camera
+    camera.update(player.position.x, player.position.y);
+    camera.draw();
+    
+
+    // DRAWING UPDATES
+    // draw player
+    player.drawEntity();
+
+    // draw weapons
+    sword.drawAttack();
+
+    // draw map
+    drawMap(0);
+
+    // drawPlayer
+    //mouseDoSomething(pressMouse);
+    // rect(x, y, playerSize, playerSize);
+
+    clicked = false;
+}
